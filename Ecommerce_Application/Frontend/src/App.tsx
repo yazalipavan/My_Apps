cd09917768cbad4c08e741852aff22f602ab450d
yazalipavan <yazalipavan@gmail.com>
2024-08-15 17:02:19 +0530

    Base pages and components

--- Ecommerce_Application/Frontend/src/App.tsx
+++ Ecommerce_Application/Frontend/src/App.tsx
@@ -1,6 +1,22 @@
-const App=()=>{
-  return(
-    <div>App</div>
+import React ,{lazy,Suspense}from 'react'
+import { BrowserRouter as Router , Routes,Route } from 'react-router-dom'
+import Loader from "./components/loader"
+
+const Home=lazy(()=>import("./pages/home"))
+const Search=lazy(()=>import("./pages/search"))
+const Cart=lazy(()=>import("./pages/cart"))
+const App = () => {
+  return (
+    <Suspense fallback={<Loader/>}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path ="/search" element={<Search/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    </Router>
+    </Suspense>
   )
 }
+
 export default App
